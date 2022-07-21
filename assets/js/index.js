ed0a0dca5d36f852a1f54c13718df45127ff14fe
DESKTOP-V8JR4HR\user <billyprayogi24@gmail.com>
2022-07-22 03:30:27 +0700

    html

---
+++ assets/js/index.js
@@ -0,0 +1,41 @@
+function getData() {
+
+let name = document.getElementById("name").value 
+let email = document.getElementById("email").value 
+let phone = document.getElementById("phone").value 
+let subject = document.getElementById("subject").value 
+let message = document.getElementById("message").value 
+
+if (!name) {
+   return alert ("name tidak boleh kosong");
+}
+else if (!email) {
+    return alert ("email tidak boleh kososng");
+}
+if (!phone) {
+   return alert ("number tidak boleh kosong");
+}
+else if (!subject) {
+   return alert ("number tidak boleh kosong");
+}
+else if (!message) {
+   return alert ("number tidak boleh kosong");
+}
+
+
+console.log(name);
+console.log(email);
+console.log(phone);
+console.log(subject);
+console.log(message); 
+
+let emailReceiver = "billyprayogi24@gmail.com"
+
+let a = document.createElement('a')
+
+a.href = `mailto:${emailReceiver}?subject=${subject}&body=Hello nama saya ${name}, ${message}, Silahkan menghubungi saya melalui email ${email} atau nomor telepon ${phone} ini. Terimakasih`
+
+a.click()
+
+
+}
